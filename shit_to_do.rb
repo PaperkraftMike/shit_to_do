28657790b3f3c2b2da889859872a9bf4fa619b3c
Michael Cahill <miccahill@gmail.com>
2013-07-24 20:16:04 -0400

    added rack flash

--- shit_to_do.rb
+++ shit_to_do.rb
@@ -5,7 +5,20 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:///shit_to_do.sqlite3"
 
 require './models.rb'
+require 'bundler/setup'
+require 'sinatra/base'
+require 'rack-flash'
 
+enable :sessions
+use Rack::Flash, :sweep => true
+
+set :sessions => true
+
+helpers do 
+  def current_user
+    session[:user_id].nil? ? nil : User.find(session[:user_id])
+    end
+ end 
 
 get '/' do
 	haml :home
@@ -19,4 +32,16 @@ end
 get '/tasks/:id' do
 	@task = Task.find(params[:id])
 	haml :task
+end
+
+get '/users/:id/task/new' do
+	@user = User.find(params[:id])
+	haml :new_task
+end
+
+post '/users/:id/task/new' do
+	@user = User.find(params[:id])
+	@task = Task.create(params)
+	@user << @task
+	redirect 'users/:id'
 end
