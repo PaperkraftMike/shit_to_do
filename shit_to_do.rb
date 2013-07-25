295e1f86fbbb10098d671c3fe3c59dddf916d06e
Alex Muniz <amuniz01@gmail.com>
2013-07-25 19:23:53 -0400

    made changes to layout

--- shit_to_do.rb
+++ shit_to_do.rb
@@ -9,3 +9,23 @@ require './models.rb'
 get '/' do
 	haml :home
 end
+
+get '/signin' do
+	haml :signin
+end
+
+get '/signup' do
+	haml :signup
+end
+
+get '/about' do
+	haml :about
+end
+
+get '/profile' do
+	haml :profile
+end
+
+get '/tasks' do
+	haml :tasks
+end
